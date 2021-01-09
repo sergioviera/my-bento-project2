aba1683227220fc390a1666f78e4ea756b07e670
Sergio Viera <sergioviera@gmail.com>
2021-01-09 13:47:55 -0300

    :wrench: Add firebase config

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,14 @@ import 'firebase/auth'
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-  apiKey: 'AIzaSyCe6CSzOUs6_Ij_VHa6hancdEi6CNBtw7c',
-  authDomain: 'bento-starter.firebaseapp.com',
-  databaseURL: 'https://bento-starter.firebaseio.com',
-  projectId: 'bento-starter',
-  storageBucket: 'bento-starter.appspot.com',
-  messagingSenderId: '458464977217'
+  apiKey: "AIzaSyCPnTCBBM1dzDtGLJATZEQySjecfxkmgO8",
+  authDomain: "viera-pwa-bento.firebaseapp.com",
+  databaseURL: "https://viera-pwa-bento-default-rtdb.firebaseio.com",
+  projectId: "viera-pwa-bento",
+  storageBucket: "viera-pwa-bento.appspot.com",
+  messagingSenderId: "483269888439",
+  appId: "1:483269888439:web:b9070991c9a018df008032",
+  measurementId: "G-TGYC07E5TJ"
 }
 
 firebase.initializeApp(config)
