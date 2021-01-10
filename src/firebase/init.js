87324b18e6d725355e682d1a1868f7b7704b240f
Sergio Viera <sergioviera@gmail.com>
2021-01-10 12:11:04 -0300

    fix file for prettier:check

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,14 +3,14 @@ import 'firebase/auth'
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-  apiKey: "AIzaSyCPnTCBBM1dzDtGLJATZEQySjecfxkmgO8",
-  authDomain: "viera-pwa-bento.firebaseapp.com",
-  databaseURL: "https://viera-pwa-bento-default-rtdb.firebaseio.com",
-  projectId: "viera-pwa-bento",
-  storageBucket: "viera-pwa-bento.appspot.com",
-  messagingSenderId: "483269888439",
-  appId: "1:483269888439:web:b9070991c9a018df008032",
-  measurementId: "G-TGYC07E5TJ"
+  apiKey: 'AIzaSyCPnTCBBM1dzDtGLJATZEQySjecfxkmgO8',
+  authDomain: 'viera-pwa-bento.firebaseapp.com',
+  databaseURL: 'https://viera-pwa-bento-default-rtdb.firebaseio.com',
+  projectId: 'viera-pwa-bento',
+  storageBucket: 'viera-pwa-bento.appspot.com',
+  messagingSenderId: '483269888439',
+  appId: '1:483269888439:web:b9070991c9a018df008032',
+  measurementId: 'G-TGYC07E5TJ'
 }
 
 firebase.initializeApp(config)
